3c7499a2473882ca5f3600ea252cc5f676370ff3
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2023-10-28 17:12:38 +0530

    retry get call on response status 500

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const getJargons = async () => {
-    const url = 'https://one00x-data-analysis.onrender.com/assignment?email=damodarnaik101@gmail.com';
-    return await fetch(url, {
-        method: 'get',
-    }).then(async response => {
-        if (response.ok) {
-            const headers = response.headers;
-            const data = await response.json();
-            return {
-                'assignmentID': response.headers.get('x-assignment-id'),
-                'jargons': data
+    try {
+        const url = 'https://one00x-data-analysis.onrender.com/assignment?email=damodarnaik101@gmail.com';
+        return await fetch(url, {
+            method: 'get',
+        }).then(async response => {
+            if (response.ok) {
+                const data = await response.json();
+                return {
+                    'assignmentID': response.headers.get('x-assignment-id'),
+                    'jargons': data
+                }
+            } else if (response.status == 500) {
+                return await getJargons();
             }
-        } else {
-            return new Error('getting jargons failed'); // Handle failure
-        }
-    }).catch(error => {
-        console.log(error)
-    })
+        })
+    } catch {
+        return new Error(error);
+    }
 }
 
 
@@ -24,45 +25,53 @@ const getJargons = async () => {
     try {
         let dataa = await getJargons();
         console.log('assignment_id: ' + dataa.assignmentID);
+        const allJargons = dataa.jargons;
         let counts = {}
         let maxUsedJargon = '';
-        dataa.jargons.forEach((item) => {
-            if (!maxUsedJargon) {
-                maxUsedJargon = item;
-            }
-            if (counts?.[item]) {
-                if ((counts[item] + 1) > Math.max(...Object.values(counts))) {
-                    maxUsedJargon = item
+        if (allJargons?.length >= 1) {
+            allJargons.forEach((item) => {
+
+                if (!maxUsedJargon) {
+                    maxUsedJargon = item;
                 }
-                counts = { ...counts, [item]: (counts[item] + 1) };
-            } else {
-                counts = { ...counts, [item]: 1 }
-            }
 
-        })
-        console.log('answer: '+ maxUsedJargon);
+                if (counts?.[item]) {
+                    if ((counts[item] + 1) > Math.max(...Object.values(counts))) {
+                        maxUsedJargon = item
+                    }
+                    counts = { ...counts, [item]: (counts[item] + 1) };
+                } else {
+                    counts = { ...counts, [item]: 1 }
+                }
 
-        const url = 'https://one00x-data-analysis.onrender.com/assignment';
-        await fetch(url, {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "assignment_id": dataa.assignmentID,
-                "answer": maxUsedJargon
-            }),
-        }).then(async response => {
+            })
+            console.log('answer: ' + maxUsedJargon);
 
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);
-            } else {
-                return new Error('submitting answer failed');
-            }
-        }).catch(error => {
-            console.log(error);
-        })
+            const url = 'https://one00x-data-analysis.onrender.com/assignment';
+            await fetch(url, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "assignment_id": dataa.assignmentID,
+                    "answer": maxUsedJargon
+                }),
+            }).then(async response => {
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data);
+                } else {
+                    return new Error('submitting answer failed');
+                }
+            }).catch(error => {
+                console.log(error);
+            })
+        } else {
+            console.log("Jargons array received is empty")
+        }
+
 
     } catch (error) {
         console.error('An error occurred:', error);
